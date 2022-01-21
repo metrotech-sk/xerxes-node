45888a53d887e6a9b0db282cd4d1f583591122e7
theMladyPan <stanislav@rubint.sk>
2022-01-21 16:04:13 +0100

    test

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from distutils.core import setup
 files = ["static/*"]
 
 setup(name = "xerxes-node",
-    version = "0.3",
+    version = "0.5",
     description = "Xerxes monitoring node app",
     author = "themladypan",
     author_email = "stanislav@rubint.sk",
     url = "rubint.sk",
-    packages = ['src'],
-    package_data = {'src' : files },
+    packages = ['xerxes'],
+    package_data = {'xerxes' : files },
     scripts = ["worker", "probe"],
     long_description = """Really long text here.""" 
 ) 
---
+++ xerxes/settings.py
@@ -0,0 +1,4 @@
+logging_level = 'INFO' # DEBUG INFO WARNING ERROR
+mongo_URI = "mongodb+srv://node:prokopcakovamama@xerxes.57jmr.mongodb.net/alfa?retryWrites=true&w=majority"
+sample_period = 1
+update_period = 60
