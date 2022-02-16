51f7095dac64e4bc6535192156b861ad3f955c6e
themladypan <stanislav@rubint.sk>
2022-02-16 10:30:30 +0000

    updated worker

--- xerxes/settings.py
+++ xerxes/settings.py
@@ -2,4 +2,6 @@ logging_level = 'DEBUG' # DEBUG INFO WARNING ERROR
 mongo_URI = "mongodb+srv://node:prokopcakovamama@xerxes.57jmr.mongodb.net/alfa?retryWrites=true&w=majority"
 sample_period = 1
 update_period = 60
-use_device = "/dev/ttyUSB0"
+use_device = "/dev/ttyS0"
+use_hw_flow = False
+hw_flow_pin_nr = 18
