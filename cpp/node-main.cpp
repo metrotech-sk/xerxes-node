364ca429ff9ed9cd3e955906e74666c38f31e1d5
MatejBartosovic <matejbart@gmail.com>
2022-02-20 21:39:39 +0100

    Real hw tuning

--- cpp/node-main.cpp
+++ cpp/node-main.cpp
@@ -1,6 +1,9 @@
 #include <Packet.h>
 #include <Serialization.h>
 #include <PacketSerializer.h>
+#include <VectorSerialization.h>
+#include <Deserialization.h>
+#include <VectorDeserialization.h>
 #include <MsgReader.h>
 #include <AsyncMsgReader.h>
 #include <rs485.h>
@@ -16,24 +19,23 @@ void sig_handler(int signum){
 int main(int argc, char** argv){
     signal(SIGINT,sig_handler);
     signal(SIGTERM,sig_handler);
-    std::shared_ptr<ABus> bus = std::make_shared<Xerxes::RS485>("");
+    std::shared_ptr<ABus> bus = std::make_shared<Xerxes::RS485>("/dev/ttyUSB0");
     AsyncMsgReader<MsgReader> asyncMsgReader(MsgReader(bus, std::chrono::milliseconds(10)),{});
-    RequestResponseClient<MsgReader, uint8_t, uint8_t> (1, 2, bus, asyncMsgReader);
-    std::vector<RequestResponseClient<MsgReader, uint8_t, uint8_t>> clients = {
-            RequestResponseClient<MsgReader, uint8_t, uint8_t> (1, 2, bus, asyncMsgReader)};
+    //todo initializer list
+    std::vector<RequestResponseClient<MsgReader, uint8_t, std::array<uint8_t, 2>>> clients; //= {
+//            std::move(RequestResponseClient<MsgReader, uint8_t, uint8_t> (0, 6, bus, asyncMsgReader))};
+    clients.emplace_back(RequestResponseClient<MsgReader, uint8_t, std::array<uint8_t, 2>> (0, 6, bus, asyncMsgReader));
     UpdateExecutor executor(asyncMsgReader);
     while (!terminate)
     {
         const auto start = std::chrono::high_resolution_clock::now();
-        std::cout << std::endl << std::endl;
-
         for(auto& client : clients)
         {
-            auto res = client.call(5);
-            res.wait();
-            std::cout << res.get() << std::endl;
+            auto res = client.call(2);
+            std::cout << "Received: " << std::hex << int(res->wait(std::chrono::milliseconds(900))[0]) << std::endl;
         }
         std::this_thread::sleep_for(std::chrono::seconds(1) - (std::chrono::high_resolution_clock::now() - start));
     }
+
     return 0;
 }
