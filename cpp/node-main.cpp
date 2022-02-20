527137e8038ababc11898d41de13813f85299a43
MatejBartosovic <matejbart@gmail.com>
2022-02-20 22:07:04 +0100

    Deserialization of Packet

--- cpp/node-main.cpp
+++ cpp/node-main.cpp
@@ -1,9 +1,9 @@
 #include <Packet.h>
 #include <Serialization.h>
-#include <PacketSerializer.h>
 #include <VectorSerialization.h>
 #include <Deserialization.h>
 #include <VectorDeserialization.h>
+#include <PacketSerializer.h>
 #include <MsgReader.h>
 #include <AsyncMsgReader.h>
 #include <rs485.h>
@@ -16,15 +16,23 @@ void sig_handler(int signum){
     terminate = true;
 }
 
+template<typename T, size_t size>
+std::ostream& operator<<(std::ostream& stream, const std::array<T, size>& data) {
+    for(const auto& i : data){
+        stream << std::hex << int(i) << ", ";
+    }
+    return stream;
+}
+
 int main(int argc, char** argv){
     signal(SIGINT,sig_handler);
     signal(SIGTERM,sig_handler);
     std::shared_ptr<ABus> bus = std::make_shared<Xerxes::RS485>("/dev/ttyUSB0");
     AsyncMsgReader<MsgReader> asyncMsgReader(MsgReader(bus, std::chrono::milliseconds(10)),{});
     //todo initializer list
-    std::vector<RequestResponseClient<MsgReader, uint8_t, std::array<uint8_t, 2>>> clients; //= {
+    std::vector<RequestResponseClient<MsgReader, uint8_t, Packet<std::array<uint8_t, 2>>>> clients; //= {
 //            std::move(RequestResponseClient<MsgReader, uint8_t, uint8_t> (0, 6, bus, asyncMsgReader))};
-    clients.emplace_back(RequestResponseClient<MsgReader, uint8_t, std::array<uint8_t, 2>> (0, 6, bus, asyncMsgReader));
+    clients.emplace_back(RequestResponseClient<MsgReader, uint8_t, Packet<std::array<uint8_t, 2>>> (0, 6, bus, asyncMsgReader));
     UpdateExecutor executor(asyncMsgReader);
     while (!terminate)
     {
@@ -32,7 +40,7 @@ int main(int argc, char** argv){
         for(auto& client : clients)
         {
             auto res = client.call(2);
-            std::cout << "Received: " << std::hex << int(res->wait(std::chrono::milliseconds(900))[0]) << std::endl;
+            std::cout << "Received: " << std::hex << res->wait(std::chrono::milliseconds(1000)).data << std::endl;
         }
         std::this_thread::sleep_for(std::chrono::seconds(1) - (std::chrono::high_resolution_clock::now() - start));
     }
