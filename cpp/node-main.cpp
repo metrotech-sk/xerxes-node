093a86803635c7c2caccb2035a9a9cc679460f2b
MatejBartosovic <matejbart@gmail.com>
2022-02-21 18:17:43 +0100

    Update node to latest protocol fixes

--- cpp/node-main.cpp
+++ cpp/node-main.cpp
@@ -1,15 +1,19 @@
+//serialization stuf
 #include <Packet.h>
-#include <Serialization.h>
+#include <PacketCrc.h>
 #include <VectorSerialization.h>
-#include <Deserialization.h>
 #include <VectorDeserialization.h>
 #include <PacketSerializer.h>
+
+//protocol stuff
 #include <MsgReader.h>
 #include <AsyncMsgReader.h>
-#include <rs485.h>
 #include <RequestResponseClient.h>
-#include <chrono>
+#include <Packet.hpp>
+
+#include <rs485.h>
 #include <signal.h>
+#include <atomic>
 
 std::atomic<bool> terminate = false;
 void sig_handler(int signum){
@@ -25,23 +29,24 @@ std::ostream& operator<<(std::ostream& stream, const std::array<T, size>& data) 
 }
 
 int main(int argc, char** argv){
+
     signal(SIGINT,sig_handler);
     signal(SIGTERM,sig_handler);
     std::shared_ptr<ABus> bus = std::make_shared<Xerxes::RS485>("/dev/ttyUSB0");
     AsyncMsgReader<MsgReader> asyncMsgReader(MsgReader(bus, std::chrono::milliseconds(10)),{});
     //todo initializer list
-    std::vector<RequestResponseClient<MsgReader, uint8_t, Packet<std::array<uint8_t, 2>>>> clients; //= {
+    std::vector<RequestResponseClient<MsgReader, uint8_t, std::array<uint8_t, 2>>> clients; //= {
 //            std::move(RequestResponseClient<MsgReader, uint8_t, uint8_t> (0, 6, bus, asyncMsgReader))};
-    clients.emplace_back(RequestResponseClient<MsgReader, uint8_t, Packet<std::array<uint8_t, 2>>> (0, 6, bus, asyncMsgReader));
+    clients.emplace_back(RequestResponseClient<MsgReader, uint8_t, std::array<uint8_t, 2>> (0, 6, bus, asyncMsgReader));
     UpdateExecutor executor(asyncMsgReader);
     while (!terminate)
     {
         const auto start = std::chrono::high_resolution_clock::now();
         for(auto& client : clients)
         {
-            auto res = client.call(2);
+            auto res = client.call(34);
             auto msg = res->wait(std::chrono::milliseconds(1000));
-            std::cout << "From: " << std::hex << int(msg.header.srcAdr) << " Data: "<< msg.data << std::endl;
+            std::cout << "From: " << std::hex << int(msg.header.srcAdr) << " Data: " << msg.data << std::endl;
         }
         std::this_thread::sleep_for(std::chrono::seconds(1) - (std::chrono::high_resolution_clock::now() - start));
     }
