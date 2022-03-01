e25b6fcaf6a7f349ce0def8ad5c3a59351ea861c
themladypan <stanislav@rubint.sk>
2022-03-01 15:48:08 +0000

    testovanie v office

--- xerxes_node/config.py
+++ xerxes_node/config.py
@@ -3,11 +3,11 @@ from xerxes_node.leaves.pleaf import Medium
 
 logging_level = 'DEBUG' # DEBUG INFO WARNING ERROR
 mongo_URI = "mongodb+srv://node:prokopcakovamama@xerxes.57jmr.mongodb.net/alfa?retryWrites=true&w=majority"
-use_database = "alpha"
+use_database = "pri_sajbach"
 sample_period = 1
 update_period = 60
 use_device = "/dev/ttyS0"
-used_medium = Medium.SILOXANE
+used_medium = Medium.WATER
 leaves = {
     0x01: "nivelation",
     0x02: "nivelation",
